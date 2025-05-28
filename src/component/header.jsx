67ff814b3994ea08738f195f6a1f5adf32b670ae
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-05-28 19:54:09 +0200

    upgrade final header

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -13,11 +13,12 @@ export default function Header() {
                   <div
                     className="collapse position-absolute bg-dark rounded shadow w-100"
                     id="navbarToggleExternalContent"
+                    style={{ zIndex: 1050, top: '100%' }}
                   >
                     <div className=" cardBurgerMenu bg-dark rounded">
                         <ul className="burgerList 
                         text-start nav  flex-column 
-                        ps-5 mt-5 pt-3 pb-2">
+                        ps-5 pt-3 pb-2">
                             <li className="nav-item"><NavLink className={({isActive}) => isActive ? "nav-link text-white active-link" : "nav-link text-white"} to="/">HOME</NavLink></li>
                             <li className="nav-item"><NavLink className={({isActive}) => isActive ? "nav-link text-white active-link" : "nav-link text-white"} to="/Service">SERVICES</NavLink></li>
                             <li className="nav-item"><NavLink className={({isActive}) => isActive ? "nav-link text-white active-link" : "nav-link text-white"} to="/Portfolio">PORTFOLIO</NavLink></li>
