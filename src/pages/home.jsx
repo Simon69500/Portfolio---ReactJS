e74fb9a2511a7b172d1fd03e2980221862d6e8ce
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-05-29 18:15:22 +0200

    responsive home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,9 +7,8 @@ import '../sass/pages/home.scss';
 export default function Home() {
 return(
     <div>
-    <Header/>
-    <main>
-
+    <main className="h-100 w-100 bg-light">
+        <Header/>
 
          {/* Contenu du haut de la page */}
         <div className="card text-bg-dark rounded-0" style={{ height: "70vh" }}>
@@ -32,38 +31,39 @@ return(
 
 
         {/* Contenu du milieu de la page - A propos */}
-        <article className="card p-4 m-3"
+        <article className="cardArticle card p-4 m-4 shadow-sm d-md-flex flex-md-row justify-content-md-center  p-md-0  mx-auto"
                   style={{boxShadow: '8px 0 12px -6px rgba(108, 117, 125, 0.3)', border: '1px solid #dee2e6'}}                
         >
-            <section className="d-flex flex-column align-items-start">
-                <h3 className="card-title border-bottom border-primary border-3 w-100 pb-3 mt-3 text-start fw-bold">A propos</h3>
-                <img src="/img/john-doe-about.jpg" className="card-img-top pt-4" alt="un homme pensif sur son bureau de travail"/>
+            <section className="articleSection d-flex flex-column align-items-start 
+                                d-md-flex flex-md-column align-items-md-center col-md-6 mx-auto 
+            ">
+                <h3 className="card-title border-bottom border-primary border-3 w-100 pb-3 mt-3 text-start fw-bold  ">A propos</h3>
+                <img src="/img/john-doe-about.jpg" className="card-img-top pt-4 img-fluid " alt="un homme pensif sur son bureau de travail"/>
                 <p className="text-start mt-3">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo odio eveniet, 
-                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos sint odit possimus sunt deleniti, 
-                    saepe veritatis suscipit. Delectus, quia sequi?
+                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos.
                 </p>
                    <p className="text-start">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo odio eveniet, 
-                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos sint odit possimus sunt deleniti, 
-                    saepe veritatis suscipit. Delectus, quia sequi?
+                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos. 
                 </p>
                    <p className="text-start">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo odio eveniet, 
-                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos sint odit possimus sunt deleniti, 
-                    saepe veritatis suscipit. Delectus, quia sequi?
+                    temporibus quisquam magni dolorum accusantium nostrum consectetur dignissimos.
                 </p>
             </section>
 
 
             {/* Contenu de fin de la page - Mes compétences */}
-            <section className="d-flex flex-column align-items-start pb-4">
+            <section className="articleSection d-flex flex-column align-items-start pb-4 
+                                d-md-flex flex-md-column align-items-md-center col-md-6 mx-auto 
+            ">
                 <h3 className="card-title border-bottom border-primary border-3 w-100 pb-3 mt-3 text-start fw-bold">Mes compétences</h3>
 
                     {/* HTML5 */}
-                <div className="w-100 mb-3 pt-5">
+                <div className="w-100 mb-3 pt-5 pt-md-3">
                     <div className="d-flex justify-content-between">
-                    <span>HTML5 90%</span>
+                    <span className="textProgress">HTML5 90%</span>
                     </div>
                     <div class="progress" role="progressbar" aria-label="Danger example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
                         <div class="progress-bar bg-danger" style={{ width: "90%" }}></div>
@@ -112,8 +112,8 @@ return(
 
             </section>
         </article>
-    </main>
     <Footer/>
+    </main>
     <Modal/>
     </div>
 )
