4315eea4c88efadc395fd3d3bf982d8383e3cf83
Simon69500 <150716564+Simon69500@users.noreply.github.com>
2025-05-29 19:04:16 +0200

    responsive Modal+ContentModal

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -6,11 +6,11 @@ export default function Modal() {
     return (
         <>
         <div className="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div className="modal-dialog modal-dialog-centered modal-xl">
+        <div className="modal-dialog modal-dialog-centered modal-lg">
             <div className="modal-content bg-dark text-white m-4 rounded-3 border border-1 border-secondary  mx-auto">
             <div className="modal-header border-secondary">
                 <h1 className="modal-title fs-5" id="staticBackdropLabel">Mon profil GitHub</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" className="btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
                 <ContentModal/>
